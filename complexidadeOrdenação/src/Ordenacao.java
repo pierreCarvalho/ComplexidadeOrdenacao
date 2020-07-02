7b3688e88296a30feb663cc9e5f38534495bb169
Pierre Carvalho <pierre.fenner@gestaods.com.br>
2020-07-02 01:09:49 -0300

    Foi adicionado o calculo de complexidade, onde calculará o tempo,uso de memoria e numero de instruções de cada metodo de ordenação adicionado anteriormente. Também foi adicionado a função para calcular o número de instruções de cada metodo de ordenação

--- complexidadeOrdenação/src/Ordenacao.java
+++ complexidadeOrdenação/src/Ordenacao.java
@@ -38,17 +38,16 @@ public class Ordenacao {
         int tmp = 0; //para as trocas
 
         for (i = 0; i < lista.size(); i++) {
-            posMenor = i;
+            posMenor = i;//1
             for (j = i + 1; j < lista.size(); j++) {
-
                 if (lista.get(j) < lista.get(posMenor)) {
-                    posMenor = j;
+                    posMenor = j;//1
                 }
             }
             if (i != posMenor) {
-                tmp = lista.get(i);
-                lista.set(i, lista.get(posMenor));
-                lista.set(i, tmp);
+                tmp = lista.get(i);//1
+                lista.set(i, lista.get(posMenor));//2
+                lista.set(i, tmp);//2
             }
         }
     }
@@ -68,80 +67,80 @@ public class Ordenacao {
     }
 
     public static void agitacao(ArrayList<Integer> lista) {
-        int i, ini = 0, fim = lista.size();
+        int i, ini = 0, fim = lista.size();//2
         boolean houveTroca;
-        int tmp = 0;
+        int tmp = 0;//1
 
         do {
             //da esquerda para direita
-            houveTroca = false; //marca que nao houveTroca
+            houveTroca = false;//1 //marca que nao houveTroca
             for (i = 0; i < fim - 1; i++) {
                 if (lista.get(i) > lista.get(i + 1)) {
-                    tmp = lista.get(i);
-                    lista.set(i, lista.get(i + 1));
-                    lista.set(i + 1, tmp);
-                    houveTroca = true;
+                    tmp = lista.get(i);//1
+                    lista.set(i, lista.get(i + 1));//2
+                    lista.set(i + 1, tmp);//2
+                    houveTroca = true;//1
                 }
             }
-            fim--;
+            fim--;//1
             if (!houveTroca) {
                 break;
             }
-
             //da direita para esquerda
-            houveTroca = false; //marca que nao houveTroca
+            houveTroca = false; //1 //marca que nao houveTroca
             for (i = fim; i > ini; i--) {
                 if (lista.get(ini) < lista.get(i - 1)) {
-                    tmp = lista.get(i);
-                    lista.set(i, lista.get(i - 1));
-                    lista.set(i - 1, tmp);
-                    houveTroca = true;
+                    tmp = lista.get(i);//1
+                    lista.set(i, lista.get(i - 1));//2
+                    lista.set(i - 1, tmp);//2
+                    houveTroca = true;//1
                 }
             }
-            ini++;
+            ini++;//1
         } while (houveTroca && ini <= fim);
     }
 
     public static void pente(ArrayList<Integer> lista) {
-	int i, distancia = lista.size();
+	int i, distancia = lista.size();//1
         boolean houveTroca;
-        int tmp = 0;
+        int tmp = 0;//1
 
         do {
-            distancia /= 1.3;
+            distancia /= 1.3; //1
             if (distancia < 1) {
-                distancia = 1;
+                distancia = 1;//1
             }
 
-            houveTroca = false; //marca que nao houveTroca
+            houveTroca = false; //1 //marca que nao houveTroca
             for (i = 0; i + distancia < lista.size(); i++) {
                 if (lista.get(i) > lista.get(i + distancia)) {
-                    tmp = lista.get(i);
-                    lista.set(i, lista.get(i + distancia));
-                    lista.set(i + distancia, tmp);
-                    houveTroca = true;
+                    tmp = lista.get(i);//1
+                    lista.set(i, lista.get(i + distancia));//2
+                    lista.set(i + distancia, tmp);//2
+                    houveTroca = true;//1
                 }
             }
         } while (distancia != 1 || houveTroca);
     }
     
     public static void shell(ArrayList<Integer> lista) {
-        int i, j, distancia = 1;
-        do {
-            distancia = distancia * 3 + 1;
+        int i, j, distancia = 1;//1
+        
+        do {//n + 1
+            distancia = distancia * 3 + 1;//1
         } while (distancia < lista.size());
         
-        int tmp = 0; //para as trocas
+        int tmp = 0; //para as trocas //1
         
         do {
-            distancia = distancia / 3;
+            distancia = distancia / 3;//1
             for (i = distancia; i < lista.size(); i++) {
-                tmp = lista.get(i);
+                tmp = lista.get(i);//1
 
                 for (j = i - distancia; j >= 0 && tmp < lista.get(j); j--) {
-                    lista.set(j + distancia, lista.get(j));
+                    lista.set(j + distancia, lista.get(j)); //2
                 }
-                lista.set(j + distancia, tmp);
+                lista.set(j + distancia, tmp);//2
             }
         }while (distancia > 1);
     }
--- complexidadeOrdenação/src/Principal.java
+++ complexidadeOrdenação/src/Principal.java
@@ -1,6 +1,6 @@
 
 import java.util.ArrayList;
-
+import java.util.Collections;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,19 +11,137 @@ import java.util.ArrayList;
  * @author pierr
  */
 public class Principal {
-
+    
+    static int NUMEXECUCOES=10;
+    //vetores como atributos da classe
+    int[] vetA, vetB, vetR;
+    
     /**
-     * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    
+	
+    /**
+     * Retorna o número de instruções conforme o polinômio da complexidade
+     * do método "somaVetores"
+     * @param tamanho - (representa o N) é mesmo do método "somaVetores"
+     * @return 2N+3
+     */
+    public static Long numInstrucoes(int tamanho,String escolha) {
+        long valor = 0;
+        if(escolha.equals("bolha")){
+            //calcular o num de instruções do bolha
+            valor = (7*(tamanho*tamanho)) + (9*(tamanho )) + 3;
+            return valor ;
+        }
+        else if(escolha.equals("seleção")){
+            valor = (2*(tamanho*tamanho))+ (8*tamanho) + 2 ;
+            return valor;
+        }
+        else if(escolha.equals("shell")){
+            valor = (3*(tamanho*tamanho*tamanho)) + (8*(tamanho*tamanho)) + (11*(tamanho)) + 9;
+            return valor;
+        }
+        else if(escolha.equals("agitacao")){
+            valor = (14*(tamanho*tamanho)) + (19*(tamanho)) + 8;
+            return valor;
+        }
+        else if(escolha.equals("pente")){
+            valor = (7*(tamanho*tamanho))+ (4*(tamanho)) + 13;
+            return valor;
+        }
+        return valor;
+    }
+    
+    public static void calcularComplexidade(String escolha){
+        
         // TODO code application logic here
         Ordenacao ord = new Ordenacao();
-        ArrayList<Integer> lista = new ArrayList<Integer>();
         
-        ord.popularLista(lista,40);
-        ord.exibirLista(lista);
-        ord.bolha(lista);
-        ord.exibirLista(lista);
+        
+        ArrayList<Long> listaMediaTempo = new ArrayList();
+        ArrayList<Long> listanumInstrucoes = new ArrayList();
+        ArrayList<Long> listamemoriaUtilizada = new ArrayList();
+        
+        //Vetor para armazenar o tempo de execução nas "NUMEXECUCOES" vezes
+        long[] temposExecucao = new long[NUMEXECUCOES];
+        int tamanhoVetores = 10;
+
+        Runtime run = Runtime.getRuntime();
+        //memória usada pelo processo atual
+        long memoriaUsada = run.totalMemory();
+        //Toda a memória disponível para a VM do Java
+        long memoriaLivre = run.freeMemory();
+
+        //dentro do for
+        int x=0;
+        long somaTempos=0;
+        long memoriaUtilizada;
+        for(int i = 100; i <= 500 ; i++){
+            memoriaUsada = run.totalMemory();
+            //Toda a memória disponível para a VM do Java
+            memoriaLivre = run.freeMemory();
+            
+            ArrayList<Integer> lista = new ArrayList<Integer>();          
+            ord.popularLista(lista,i);
+            
+            for(int exec=0;exec<NUMEXECUCOES;exec++) {
+                long inicio = System.nanoTime();
+                if(escolha.equals("bolha")){
+                    ord.bolha(lista);
+                }              
+                long fim = System.nanoTime();
+                long tempoExec = fim -inicio;
+                temposExecucao[exec] = tempoExec;
+            }
+
+            memoriaUtilizada = memoriaUsada - memoriaLivre;                 
+            while(x<temposExecucao.length) {
+                somaTempos = somaTempos + temposExecucao[x];
+                x++;
+            }
+            long mediaTempos = somaTempos / NUMEXECUCOES;
+            listaMediaTempo.add(mediaTempos);
+            listanumInstrucoes.add(numInstrucoes(i,escolha));
+            listamemoriaUtilizada.add(memoriaUtilizada);
+            x = 0;
+            somaTempos=0;
+            memoriaUtilizada = 0;
+            memoriaUsada = 0;
+            memoriaLivre = 0;
+        }
+        
+        System.out.println("lista media do tempo do "+escolha);
+        System.out.print("[");
+        for (int i = 0; i < listaMediaTempo.size(); i++) {
+            System.out.print(""+listaMediaTempo.get(i)+",");
+        }
+        System.out.println("]");
+        
+        System.out.println("lista numero instrucoes do "+escolha);
+        System.out.print("[");
+        for (int i = 0; i < listanumInstrucoes.size(); i++) {
+            System.out.print(""+listanumInstrucoes.get(i)+",");
+        }
+        System.out.println("]");
+        
+        System.out.println("lista memoria utilizada do"+escolha);
+        System.out.print("[");
+        for (int i = 0; i < listamemoriaUtilizada.size(); i++) {
+            System.out.print(""+listamemoriaUtilizada.get(i)+",");
+        }
+        System.out.print("]");
+        System.out.println("---------------------------------------");
+    }
+
+    public static void main(String[] args) {
+        
+        
+        calcularComplexidade("bolha");
+        //calcularComplexidade("bolha");
+        //calcularComplexidade("bolha");
+        //calcularComplexidade("bolha");
+        //calcularComplexidade("bolha");
+
     }
     
 }
